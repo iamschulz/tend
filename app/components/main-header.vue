dd1b38ccd0b52aaee338001d41462a453c55d7b5
iamschulz <hallo@iamschulz.de>
2026-03-01 22:36:00 +0100

    add 404 page

--- app/components/main-header.vue
+++ app/components/main-header.vue
@@ -41,9 +41,12 @@ const handleMenuButtonClick = (): void => {
     box-shadow: 0 calc(5px * var(--scrolled)) calc(5px * var(--scrolled)) 0 var(--col-bg3);
     font-size: calc(1rem * (1 - var(--scrolled) * 0.2));
     padding-block: calc(1rem * (1 - var(--scrolled) * 0.8));
-    animation: scrolled ease-out both;
-    animation-timeline: scroll();
-    animation-range: 0px 20px;
+
+    @supports (animation-timeline: scroll()) {
+        animation: scrolled ease-out both;
+        animation-timeline: scroll();
+        animation-range: 0px 20px;
+    }
 }
 
 .header-inner {
---
+++ app/error.vue
@@ -0,0 +1,26 @@
+<template>
+    <main>
+        <ErrorNotice>
+            {{ error?.statusCode === 404 ? $t('pageNotFoundMessage') : $t('somethingWrong') }}
+            <div class="error-action">
+                <NuxtLink href="/" @click="() => clearError()">{{ $t('goBack') }}</NuxtLink>
+            </div>
+        </ErrorNotice>
+    </main>
+</template>
+
+<script setup lang="ts">
+    import type { NuxtError } from '#app'
+
+    defineProps<{
+        error: NuxtError
+    }>()
+</script>
+
+<style scoped>
+    .error-action {
+        max-width: var(--narrow-width);
+        margin: 0 auto;
+        text-align: center;
+    }
+</style>
